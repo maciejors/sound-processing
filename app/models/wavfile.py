27d8a19cac33d54ed796e783c373bcec455f85a4
maciejors <maciej.orslowski@gmail.com>
2023-03-28 17:47:22 +0200

    add audio normalisation

--- app/models/wavfile.py
+++ app/models/wavfile.py
@@ -5,14 +5,15 @@ import numpy as np
 
 class WavFile:
 
-    __slots__ = ['samples', 'n_channels', 'n_samples', 'sample_rate']
+    __slots__ = ['samples', 'n_channels', 'n_samples', 'sample_rate', 'sample_width']
 
-    def __init__(self, file):
+    def __init__(self, file, normalise: bool = True):
         with wave.open(file, mode='rb') as wavfile_raw:
             # basic audio properties:
             self.n_channels = wavfile_raw.getnchannels()
             self.n_samples = wavfile_raw.getnframes()
             self.sample_rate = wavfile_raw.getframerate()
+            self.sample_width = wavfile_raw.getsampwidth()
 
             samples_raw = wavfile_raw.readframes(-1)
             samples_all_channels = np.frombuffer(samples_raw, dtype=np.int16)
@@ -26,6 +27,19 @@ class WavFile:
             # merge them as rows in the final samples array:
             self.samples = np.array(channels)
 
+            if normalise:
+                self.__normalise_samples()
+
     @property
     def audio_length_sec(self) -> float:
         return self.n_samples / self.sample_rate
+
+    def __normalise_samples(self):
+        max_amplitude = np.max(self.samples)
+        target_level_db = -3
+        target_amplitude = 10 ** (target_level_db / 20) * (2 ** (self.sample_width * 8 - 1) - 1)
+
+        gain = target_amplitude / max_amplitude
+        samples_normalised = np.floor(self.samples * gain)
+
+        self.samples = samples_normalised
