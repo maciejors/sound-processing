dcc297938c50fe106e1325bedbe8d5c3902a680e
maciejors <maciej.orslowski@gmail.com>
2023-04-21 13:55:33 +0200

    change silence ratio plot to a line plot

--- app/pages/1_Frame-level_parameters.py
+++ app/pages/1_Frame-level_parameters.py
@@ -54,26 +54,29 @@ if store.get_wavfile() is not None:
 
     # classes per frame
     classes = wav.get_frame_types(zcr_threshold, volume_threshold)
-    # apply classes to samples
-    classes = np.repeat(classes, wav.frame_size)
-    class_to_color = {0: 'red', 1: 'yellow', 2: 'blue'}
     timestamps_samples = np.linspace(0, wav.audio_length_sec, num=wav.n_samples_in_frames)
-    # create a list of trace objects, one for each class
+    # create a list of trace objects, one for each frame
     traces = []
-    for cls, color in class_to_color.items():
-        # filter the x and y values for this class
-        x_cls = timestamps_samples[classes == cls]
-        y_cls = wav.samples[:wav.n_samples_in_frames][classes == cls]
-        
-        # create a scatter trace with the filtered x and y values
+    class_to_color = {0: 'red', 1: 'yellow', 2: 'blue'}
+    # the set below is used to prevent duplicate entries in the legend
+    classes_on_legend = set()
+    for i, cls in enumerate(classes):
+        # filter the x and y values for this frame
+        x_cls = timestamps_samples[i * wav.frame_size: (i + 1) * wav.frame_size]
+        y_cls = wav.samples[i * wav.frame_size: (i + 1) * wav.frame_size]
+
+        # create a line trace with the filtered x and y values
         trace = go.Scatter(
             x=x_cls,
             y=y_cls,
-            mode='markers',
-            marker=dict(color=color),
-            name="Silence" if cls == 0 else "Unvoiced" if cls == 1 else "Voiced"
+            mode='lines',
+            line=dict(color=class_to_color[cls]),
+            legendgroup=str(cls),
+            name="Silence" if cls == 0 else "Unvoiced" if cls == 1 else "Voiced",
+            showlegend=cls not in classes_on_legend
         )
-        
+        classes_on_legend.add(cls)
+
         # add the trace to the list
         traces.append(trace)
 
@@ -91,4 +94,3 @@ if store.get_wavfile() is not None:
         labels={'x': 'Time (in seconds)', 'y': 'F0'},
     )
     st.plotly_chart(plot, use_container_width=True)
-
