20f216ef3f2b042c38c8d9d92b6d86ce65197042
maciejors <maciej.orslowski@gmail.com>
2023-05-15 22:36:55 +0200

    add windowing

---
+++ app/core/windowing.py
@@ -0,0 +1,59 @@
+from typing import Callable
+
+import numpy as np
+import scipy.signal.windows as wn
+
+from app.models.signal import Signal
+
+
+class Windowing:
+    """
+    Class that provides methods to handle signal windowing.
+    """
+
+    __slots__ = ['_signal', 'windowing_func']
+
+    def __init__(self, signal: Signal):
+        self._signal = signal
+        # default window func
+        self.windowing_func = wn.hamming
+
+    @property
+    def function_map(self) -> dict[str, Callable]:
+        """Returns a dict that maps user-friendly windowing function names to
+        actual function objects"""
+        return {
+            'Rectangular': wn.boxcar,
+            'Triangular': wn.triang,
+            'Bartlett': wn.bartlett,
+            'Blackman': wn.blackman,
+            'van Hann': wn.hann,
+            'Hamming': wn.hamming,
+            'Parzen': wn.parzen,
+            'Taylor': wn.taylor,
+        }
+
+    def set_windowing_func(self, func_name: str):
+        try:
+            self.windowing_func = self.function_map[func_name]
+        except KeyError:
+            raise ValueError('No windowing function with that name available. '
+                             f'Available functions: {self.function_map.keys()}')
+
+    def windowed_samples(self) -> np.ndarray:
+        """Applies windowing to original samples"""
+        window = self.windowing_func(self._signal.n_samples)
+        return self._signal.samples * window
+
+    def windowed_fft_freqs(self) -> np.ndarray:
+        """Returns x-axis of frequency domain plot for a signal with windowing applied"""
+        freq_vals = np.fft.fftfreq(
+            len(self.windowed_samples()),
+            1 / self._signal.sample_rate
+        )
+        return freq_vals
+
+    def windowed_fft_magn_spectr(self) -> np.ndarray:
+        """Returns fft magnitude spectrum for a signal with windowing applied"""
+        fft_result = np.fft.fft(self.windowed_samples())
+        return np.abs(fft_result)
---
+++ app/pages/4_Windowing.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+from app import store
+from app.core.windowing import Windowing
+
+pd.options.plotting.backend = "plotly"  # for multiline plots
+
+st.markdown('# Windowing')
+
+if store.get_signal() is not None:
+    signal = store.get_signal()
+    windowing = Windowing(signal)
+
+    selected_func_name = st.selectbox(
+        'Select a windowing function',
+        windowing.function_map.keys(),
+    )
+    windowing.set_windowing_func(selected_func_name)
+
+    # Signal plot
+    st.markdown('---')
+    st.markdown('## Signal plots')
+    timestamps_samples = np.linspace(0, signal.audio_length_sec, num=signal.n_samples)
+
+    original_signal_col, windowed_signal_col = st.columns([1, 1])
+    with original_signal_col:
+        plot = px.line(
+            x=timestamps_samples,
+            y=signal.samples,
+            title='Original signal',
+            labels={'x': 'Time (in seconds)', 'y': 'Amplitude'},
+        )
+        st.plotly_chart(plot, use_container_width=True)
+    with windowed_signal_col:
+        plot = px.line(
+            x=timestamps_samples,
+            y=windowing.windowed_samples(),
+            title='Windowed signal',
+            labels={'x': 'Time (in seconds)', 'y': 'Amplitude'},
+        )
+        st.plotly_chart(plot, use_container_width=True)
+
+    # Frequency plot
+    st.markdown('---')
+    st.markdown('## Frequency domain plots')
+    timestamps_samples = np.linspace(0, signal.audio_length_sec, num=signal.n_samples)
+
+    original_signal_col, windowed_signal_col = st.columns([1, 1])
+    with original_signal_col:
+        plot = px.line(
+            x=signal.fft_freqs_full,
+            y=signal.fft_magn_spectr_full,
+            title='Original frequency domain',
+            labels={'x': 'Time (in seconds)', 'y': 'Magnitude'},
+        )
+        st.plotly_chart(plot, use_container_width=True)
+    with windowed_signal_col:
+        plot = px.line(
+            x=windowing.windowed_fft_freqs(),
+            y=windowing.windowed_fft_magn_spectr(),
+            title='Frequency domain after windowing',
+            labels={'x': 'Time (in seconds)', 'y': 'Magnitude'},
+        )
+        st.plotly_chart(plot, use_container_width=True)
