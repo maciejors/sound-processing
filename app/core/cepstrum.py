29e358efadd8c5b84875afbfec871650d70c8189
Sabina <sabinasidarovich@gmail.com>
2023-05-19 20:37:38 +0200

    cepstrum mod proposition

--- app/core/cepstrum.py
+++ app/core/cepstrum.py
@@ -15,15 +15,31 @@ class Cepstrum:
 
     @staticmethod
     def cepstrum(samples: np.ndarray) -> np.ndarray:
-        return np.fft.fft(np.log(np.abs(np.fft.fft(samples))))
+        return np.fft.ifft(np.log(np.abs(np.fft.fft(samples))))
 
     def f0(self) -> np.ndarray:
         def get_pitch_of_frame(frame: np.ndarray) -> float:
-            cepstrum = self.cepstrum(frame)
+            cepstrum = np.real(self.cepstrum(frame))
             cepstrum[0] = 0
-            # first max
-            argmax = np.argmax(np.abs(cepstrum))
-            return self._signal.sample_rate / (2 * argmax)
+
+            # Frequency bounds
+            period_ub = 1 / 50  # Highest frequency (50 Hz)
+            period_lb = 1 / 400  # Lowest frequency (400 Hz)
+
+            # Find local maxima within the frequency bounds
+            cepstrum_freqs = np.fft.fftfreq(len(cepstrum), 1 / self._signal.sample_rate)
+            mask = (cepstrum_freqs >= period_lb) & (cepstrum_freqs <= period_ub)
+            local_maxima_indices = np.where((cepstrum == np.maximum(cepstrum, np.roll(cepstrum, 1))) &
+                                            (cepstrum == np.maximum(cepstrum, np.roll(cepstrum, -1))) &
+                                            mask)[0]
+
+            if len(local_maxima_indices) > 0:
+                # Get the index of the highest local maximum
+                argmax = np.argmax(cepstrum[local_maxima_indices])
+                index = local_maxima_indices[argmax]
+                return self._signal.sample_rate / cepstrum_freqs[index] / 2
+            else:
+                return 0.0
 
         pitches = [get_pitch_of_frame(f) for f in self._signal.frames]
         return np.array(pitches)
